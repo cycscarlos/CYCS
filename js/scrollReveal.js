2b032e3749703d35153f82151562577f35d12b58
cycscarlos <cycs.ingenieria@gmail.com>
2021-10-25 22:58:46 -0400

    Ajustes para habilitar el Dark Theme

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,42 +1,31 @@
 //: SCROLL REVEAL ANIMATION */
 const sr = ScrollReveal({
   distance: "60px",
-  duration: 2800,
+  duration: 4800,
   // reset: true,
 });
 
-// sr.reveal(`.swiper-container`, {
-//   origin: "right",
-//   interval: 10,
-// });
+sr.reveal(`.cards, .security, .muro, .servicios`, {
+  origin: "top",
+  interval: 100,
+});
 
-sr.reveal(
-  `.home__data, .home__social-link, .home__info,         
-           .experience__data, .experience__overlay,
-           .place__card,           
-           .clientes__content,
-           .footer__data, .footer__rights`,
-  {
-    origin: "top",
-    interval: 100,
-  }
-);
+sr.reveal(`.ctrlAcceso, .aboutUs, .galeria`, {
+  origin: "left",
+  interval: 100,
+});
 
-sr.reveal(
-  `.about__data, 
-           .video__description,
-           .subscribe__description`,
-  {
-    origin: "left",
-  }
-);
+sr.reveal(`.telco, .clients, .servicesRD`, {
+  origin: "right",
+  interval: 100,
+});
 
-sr.reveal(
-  `.about__img-overlay, 
-           .video__content,
-           .subscribe__form`,
-  {
-    origin: "right",
-    interval: 100,
-  }
-);
+// sr.reveal(
+//   `.about__img-overlay,
+//            .video__content,
+//            .subscribe__form`,
+//   {
+//     origin: "right",
+//     interval: 100,
+//   }
+// );
